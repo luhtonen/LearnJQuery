9fd1470ea8a89aab5b926ce2adc26b2d2387b73e
Eduard Luhtonen <luhtonen@gmail.com>
2014-01-08 18:51:12 +0100

    refactoring even more

--- public_html/scripts/03.js
+++ public_html/scripts/03.js
@@ -1,17 +1,14 @@
 $(document).ready(function() {
-    $('#switcher-default')
-        .addClass('selected')
-        .on('click', function() {
-            $('body').removeClass();
-        });
-    $('#switcher-narrow').on('click', function() {
-        $('body').removeClass().addClass('narrow');
-    });
-    $('#switcher-large').on('click', function() {
-        $('body').removeClass().addClass('large');
-    });
+    $('#switcher-default').addClass('selected');
     $('#switcher button').on('click', function() {
+        $('body').removeClass();
         $('#switcher button').removeClass('selected');
         $(this).addClass('selected');
+    });
+    $('#switcher-narrow').on('click', function() {
+        $('body').addClass('narrow');
+    });
+    $('#switcher-large').on('click', function() {
+        $('body').addClass('large');
     });
 });
