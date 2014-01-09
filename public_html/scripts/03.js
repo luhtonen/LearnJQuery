4409adbdef392829ec98e5de450d92c2941953e6
Eduard Luhtonen <luhtonen@gmail.com>
2014-01-09 08:33:32 +0100

    hide/show buttons panel

--- public_html/scripts/03.js
+++ public_html/scripts/03.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+    $('#switcher h3').click(function() {
+        $('#switcher button').toggleClass('hidden');
+    });
     $('#switcher-default').addClass('selected');
     $('#switcher button').click(function() {
         var bodyClass = this.id.split('-')[1];
