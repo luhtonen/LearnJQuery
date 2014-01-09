3dc123290e06fef051c20cafc835f0148bc4c1f4
Eduard Luhtonen <luhtonen@gmail.com>
2014-01-09 08:27:58 +0100

    switching to use event shorthand

--- public_html/scripts/03.js
+++ public_html/scripts/03.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('#switcher-default').addClass('selected');
-    $('#switcher button').on('click', function() {
+    $('#switcher button').click(function() {
         var bodyClass = this.id.split('-')[1];
         $('body').removeClass().addClass(bodyClass);
         $('#switcher button').removeClass('selected');
